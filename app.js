f005791ea887b165e8402b7230e93c62f7fe2fa6
EyobaGit <juu.shashe@gmail.com>
2025-06-08 22:38:47 +0300

    onestep-ahead

--- app.js
+++ app.js
@@ -1,41 +1,33 @@
 require("dotenv").config();
 const express = require("express");
-
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
 const port = 2112;
 
-//  global middlewares that apply to all routes.
-app.use(cors())
-app.use(express.json()); // json middleware to extract json data
+// Global middlewares
+app.use(cors());
+app.use(express.json());
 
-const dbConnection = require("./Db/dbConfig");
+
+// DB connection and table schemas
+
+const dbconnection = require ('./Db/dbConfig')
 const { users, questions, answers } = require("./Table/Schema");
 
-
-
-// const userRoutes = require("./Routes/userRoute");
-// const questionRoutes = require("./Routes/questionRoute");
-const authMiddleware = require("./MiddleWare/authMiddleWare")
-
+// Routes
 const userRoutes = require("./Routes/userRoute");
-const questionRoutes = require("./routes/questionRoute");
-const authMiddleware = require("./middleware/authMiddleware");
+const questionRoutes = require("./Routes/questionRoute");
+// const authMiddleware = require("./MiddleWare/authMiddleware");
 
-
-// user Route middleware
-// app.use("/api/users", userRoutes);
-
-// !Question route middleware
+// Apply routes
+app.use("/api/users", userRoutes);
 // app.use("/api/questions", authMiddleware, questionRoutes);
 
-// !Answer route middleware
-
-// Start the server and create tables
+// Start server and create tables
 async function start() {
   try {
-    const result = await dbConnection.execute("select 'test' ");
+     await dbConnection.query("SELECT 'test'"); // Test DB connection
     console.log("Database connection established");
 
     // Create tables
@@ -46,7 +38,7 @@ async function start() {
     await app.listen(port);
 
 }
-}
+
 // start server
 
 start();
