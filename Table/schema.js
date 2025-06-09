84d3a545fd360cd97e490b280605f4887f7d02e5
nafikoo <Naifabdulfetah000@gmail.com>
2025-06-09 22:45:04 +0300

    schema

--- Table/schema.js
+++ Table/schema.js
@@ -1,4 +1,4 @@
-export let users = `CREATE TABLE IF NOT EXISTS users(
+export let users = `CREATE TABLE IF NOT EXISTS users (
     userid INT(20) NOT NULL AUTO_INCREMENT,
     username VARCHAR(20) NOT NULL,
     firstname VARCHAR(20) NOT NULL,
@@ -8,22 +8,24 @@ export let users = `CREATE TABLE IF NOT EXISTS users(
     PRIMARY KEY (userid)
 )`;
 
-export let questions = `CREATE TABLE IF NOT EXISTS questions(
-    id INT(20) NOT NULL AUTO_INCREMENT,
-    questionid VARCHAR(100) NOT NULL,
+export let questions = `CREATE TABLE IF NOT EXISTS questions (
+    question_id INT(20) NOT NULL AUTO_INCREMENT,
     userid INT(20) NOT NULL,
     title VARCHAR(50) NOT NULL,
     description VARCHAR(200) NOT NULL,
     tag VARCHAR(20),
-    PRIMARY KEY (id, questionid),
-    FOREIGN KEY(userid) REFERENCES users(userid)
-)`;
+    createdat TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (question_id),
+    FOREIGN KEY (userid) REFERENCES users(userid)
+);`;
 
-export let answers =`CREATE TABLE IF NOT EXISTS answers(
-    answerid INT(20) NOT NULL,
+export let answers = `CREATE TABLE IF NOT EXISTS answers (
+    answer_id INT(20) NOT NULL AUTO_INCREMENT,
     userid INT(20) NOT NULL,
-    questionid VARCHAR(100) NOT NULL,
+    question_id INT(20) NOT NULL,
+    createdat TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     answer VARCHAR(200) NOT NULL,
-    PRIMARY KEY (answerid),
-    FOREIGN KEY(userid) REFERENCES users(userid)
+    PRIMARY KEY (answer_id),
+    FOREIGN KEY (userid) REFERENCES users(userid),
+    FOREIGN KEY (question_id) REFERENCES questions(question_id)
 )`;
