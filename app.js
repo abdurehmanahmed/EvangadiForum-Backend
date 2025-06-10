33215bf00c77b493e2892b3a1b875dd2676051e2
nafikoo <Naifabdulfetah000@gmail.com>
2025-06-10 10:35:26 +0300

    table

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 2112;
+const port = 5000;
 
 // Global middlewares
 app.use(cors());
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // DB connection and table schemas
 const dbConnection = require("./Db/dbConfig");
-const { users, questions, answers } = require("./Table/Schema");
+const { users, questions, answers } = require("./Table/schema");
 
 // Routes
 const userRoutes = require("./Routes/userRoute");
